6fc5fe8a9335e585e5b5f0839cc312cb6699ce1e
Ximena Blancas <124410723+XimenaBlancass@users.noreply.github.com>
2023-08-29 19:29:10 -0700

    Parcial1, practica1 / 29.08.23

---
+++ Parcial1/Practica1.js
@@ -0,0 +1,23 @@
+$(document).ready(function (){
+    //alert("Funciona Jquery");
+    $("#boton1").on("click", function(){
+     $("#texto1").html("texto")
+    });
+
+    $("#texto1").hover(function () {
+        $(this).css("background-color", "blue");
+        $(this).css("font-size", "50px");
+    }, function () {
+        $(this).css("background-color", "white");
+        $(this).css("font-size", "20px");
+    }
+
+    );
+
+    $("#boton2").click(function () {
+        $("#imagen1").fadeToggle("slow");
+    }
+    );
+}
+
+);
