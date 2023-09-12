93d4e711a695b44ccd4a5c3ecf226cb4c49d4f23
Ximena Blancas <124410723+XimenaBlancass@users.noreply.github.com>
2023-09-11 19:22:57 -0700

    Practica 3 / 11.09.23

--- Parcial1/pares.js
+++ Parcial1/pares.js
@@ -8,13 +8,34 @@ $(document).ready(function(){
         var nombre_imagen = $(this).attr('data-id');
 
         if(estado == "0"){
+                //si las cartas son diferentes
+                if(carta1 !== "" && carta2 !== ""){
+                    console.log("Las cartas son diferentes")
+                    if(carta1.attr('data-id') != carta2.attr('data-id')){
+                    console.log("Tapa cartas diferentes")
+                    carta1.attr('src', 'imagenes/portada.jpg')
+                    carta2.attr('src', 'imagenes/portada.jpg')
+                    carta1.attr('data-estado', '0')
+                    carta2.attr('data-estado', '0')
+                }else {
+                    console.log("Las cartas son iguales")
+                    carta1.attr('data-id', '1')
+                    carta2.attr('data-id', '1')
+                }
+
+                carta1=""
+                carta2=""
+            }
+            console.log("Estado de la carta: " +estado)
             if(carta1 == ""){
                 carta1 = $(this);
                 carta1.attr('src', 'imagenes/' + nombre_imagen);
                 console.log("Se asigno carta #1");
+                carta1.attr('data-estado', '1')
             } else {
                 carta2 = $(this);
                 carta2.attr('src', 'imagenes/' + nombre_imagen);
+                carta2.attr('data-estado', '1')
                 console.log("Se asigno carta #2");
             }
         }
@@ -23,9 +44,19 @@ $(document).ready(function(){
                 par=true;
                 total_pares++;
                 $("#total_pares").html(total_pares);
-                carta1 = "";
-                carta2 = "";
             }
+        }
+            if(total_pares ==8){
+                $('img').each(function(){
+                    $(this).attr('src', 'imagenes/portada.jpg')
+                    $(this).attr('data-estado', '0')
+                });
+                total_pares=0
+                par=false;
+                carta1=""
+                carta2=""
+                return;
+            
         }
     });
 });
