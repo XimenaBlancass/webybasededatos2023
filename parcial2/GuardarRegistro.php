62bd49dd1ddf5c41ccb677f03e4cb9aa498c787a
Ximena Blancas <124410723+XimenaBlancass@users.noreply.github.com>
2023-10-25 19:42:02 -0700

    25.10.23 Agregamos consultar alumos y guardar registros

---
+++ parcial2/GuardarRegistro.php
@@ -0,0 +1,23 @@
+<?php 
+
+include 'conexion.php';
+$nombre = $_POST["nombre"];
+$numero_control = $_POST["nc"];
+$semestre = $_POST["semestre"];
+$edad = $_POST["edad"];
+$turno = $_POST["turno"];
+$sexo = $_POST["sexo"];
+$sql="INSERT INTO alumnos(nombre, numero_control, semestre, edad, turno, sexo) VALUES ()";
+$SQL="VALUES('".$nombre."','".$numero_control."','".$semestre."','".$edad."','".$turno."','".$sexo."')"
+
+if($conexion->query($sql)=== TRUE) {
+    header("location: ConsultarAlumnos.php");
+    $conexion->close();
+    exit;
+} else {
+    echo"<h2>Ocurrio un error</h2> <p>Error:" .$sql. "<br>" .$conexion->error. "</p>";
+    echo"<h3><a href= consultarAlumnos.php/h3>";
+
+}
+
+?>
